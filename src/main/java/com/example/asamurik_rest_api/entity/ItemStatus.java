c06cec7ec325ee24e9b1fc89cd1950be363bb7e7
ahmad alfarel <assaniry202@gmail.com>
2025-05-20 16:19:38 +0700

    feat: menambahkan enum untuk status di table Item

---
+++ src/main/java/com/example/asamurik_rest_api/entity/ItemStatus.java
@@ -0,0 +1,17 @@
+package com.example.asamurik_rest_api.entity;
+
+public enum ItemStatus {
+    FRESH("Fresh"),
+    ON_PROGRESS("On Progress"),
+    FOUND("Found");
+
+    private final String status;
+
+    ItemStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
