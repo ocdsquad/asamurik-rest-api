87a26e74e81c866815364cc4158a5a6c972fa523
krisnaajiep <krisnaajiep@gmail.com>
2025-05-24 23:32:50 +0700

    feat: menambah UserRepository interface di package repository

---
+++ src/main/java/com/example/asamurik_rest_api/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.asamurik_rest_api.repository;
+
+import com.example.asamurik_rest_api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Boolean existsByEmail(String email);
+    Boolean existsByUsername(String username);
+}
