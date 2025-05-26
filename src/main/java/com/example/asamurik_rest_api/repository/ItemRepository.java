73413c08882dcbccc53d29efbc4c9615e95bea1e
ahmad alfarel <assaniry202@gmail.com>
2025-05-26 09:52:25 +0700

    feat: menambahkan item repo karena untuk digunakan di service report

---
+++ src/main/java/com/example/asamurik_rest_api/repository/ItemRepository.java
@@ -0,0 +1,10 @@
+package com.example.asamurik_rest_api.repository;
+
+import com.example.asamurik_rest_api.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ItemRepository extends JpaRepository<Item, UUID> {
+
+}
