141516fd5e87123d822e98c9ab840a5f8e89ffbd
ahmad alfarel <assaniry202@gmail.com>
2025-05-26 09:50:56 +0700

    feat: menambahkan GlobalSuccessHandler untuk memudahkan memanggil responseSuccess untuk beberapa kondisi

--- src/main/java/com/example/asamurik_rest_api/handler/ResponseHandler.java
+++ src/main/java/com/example/asamurik_rest_api/handler/ResponseHandler.java
@@ -3,7 +3,6 @@ package com.example.asamurik_rest_api.handler;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
@@ -18,23 +17,23 @@ public class ResponseHandler {
             Object data,
             Object errorCode,
             HttpServletRequest request
-    ){
+    ) {
 
-        Map<String,Object> m = new HashMap<>();
-        m.put("message",message);
-        m.put("data",data==null?"":data);
+        Map<String, Object> m = new HashMap<>();
+        m.put("message", message);
+        m.put("data", data == null ? "" : data);
         m.put("timestamp", LocalDateTime.now().toString());
-        m.put("success",!status.isError());
-        if(errorCode!=null){
-            m.put("error-code",errorCode);
-            m.put("path",request.getRequestURI());
+        m.put("success", !status.isError());
+        if (errorCode != null) {
+            m.put("error-code", errorCode);
+            m.put("path", request.getRequestURI());
         }
-        return new ResponseEntity<>(m,status);
+        return new ResponseEntity<>(m, status);
     }
 
     /*Contoh penggunaan success response
-    * return responseHandler.handleResponse(HttpStatus status, SuccessCode.SUCCESS.getMessage(), data, null, request);
-    * */
+     * return responseHandler.handleResponse(SuccessCode.SUCCESS.getMessage(), HttpStatus.CREATED, data, null, request);
+     * */
 
     public ResponseEntity<Object> handleResponse(
             String message,
@@ -42,22 +41,22 @@ public class ResponseHandler {
             Object data,
             Object errorCode,
             WebRequest request
-    ){
+    ) {
 
-        Map<String,Object> m = new HashMap<>();
-        m.put("message",message);
+        Map<String, Object> m = new HashMap<>();
+        m.put("message", message);
 //        m.put("status",status.value());
-        m.put("data",data==null?"":data);
+        m.put("data", data == null ? "" : data);
         m.put("timestamp", LocalDateTime.now().toString());
-        m.put("success",!status.isError());
-        if(errorCode!=null){
-            m.put("error-code",errorCode);
-            m.put("path",request.getContextPath());
+        m.put("success", !status.isError());
+        if (errorCode != null) {
+            m.put("error-code", errorCode);
+            m.put("path", request.getContextPath());
         }
-        return new ResponseEntity<>(m,status);
+        return new ResponseEntity<>(m, status);
     }
 
     /*Contoh penggunaan error response
-     * return responseHandler.handleResponse(status, ErrorCode.INTERNAL_SERVER_ERROR.getMessage(), null, HttpStatus.INTERNAL_SERVER_ERROR, request);
+     * return responseHandler.handleResponse(status, HttpStatus.INTERNAL_SERVER_ERROR, null, ErrorCode.INTERNAL_SERVER_ERROR.getMessage(), request);
      * */
 }
