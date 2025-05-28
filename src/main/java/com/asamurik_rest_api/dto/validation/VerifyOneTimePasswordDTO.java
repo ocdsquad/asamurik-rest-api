fb75e7d45dd5a703555b37e6af1ee8a7b0c86da3
krisnaajiep <krisnaajiep@gmail.com>
2025-05-28 20:20:16 +0700

    refactor: menghapus OneTimePasswordDTO.java dan rename VerifyRegistrationDTO -> VerifyOneTimePasswordDTO

--- src/main/java/com/asamurik_rest_api/dto/validation/VerifyOneTimePasswordDTO.java
+++ src/main/java/com/asamurik_rest_api/dto/validation/VerifyOneTimePasswordDTO.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
-public class VerifyRegistrationDTO {
+public class VerifyOneTimePasswordDTO {
     @NotNull(message = "Email tidak boleh null")
     @Size(max = 100, message = "Email maksimal 100 karakter")
     @Pattern(
