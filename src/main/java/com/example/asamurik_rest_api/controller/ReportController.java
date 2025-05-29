99fcc9815bbf2c6d578ca0006eb3f7a73cb2b977
ahmad alfarel <assaniry202@gmail.com>
2025-05-29 15:23:19 +0700

    feat: add report untuk guest dan auth user

--- src/main/java/com/example/asamurik_rest_api/controller/ReportController.java
+++ src/main/java/com/example/asamurik_rest_api/controller/ReportController.java
@@ -2,7 +2,9 @@ package com.example.asamurik_rest_api.controller;
 
 
 import com.example.asamurik_rest_api.dto.validation.ValidateReportDTO;
+import com.example.asamurik_rest_api.dto.validation.ValidateReportGuestDTO;
 import com.example.asamurik_rest_api.service.ReportService;
+import com.example.asamurik_rest_api.utils.JwtUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +16,25 @@ import org.springframework.web.bind.annotation.*;
 public class ReportController {
     @Autowired
     private ReportService reportService;
+    @Autowired
+    private JwtUtil jwtUtil;
 
     @PostMapping("/{itemID}")
-    public ResponseEntity<Object> createReport(@Valid @RequestBody ValidateReportDTO reportDTO,
-                                               HttpServletRequest request, @PathVariable String itemID) {
-        String token = request.getHeader("Authorization").substring(7); // Remove "Bearer " prefix
-        String userID = "jwtTokenUtil.validateAndExtractUserId(token)";
-        return reportService.save(reportService.mapToReport(reportDTO), request, itemID, userID);
+    public ResponseEntity<Object> sendReport(@Valid @RequestBody ValidateReportDTO reportDTO,
+            @RequestHeader("Authorization") String token, HttpServletRequest request, @PathVariable String itemID) {
+        if(token != null && jwtUtil.validateToken(token)){
+            String userID = jwtUtil.getUserIdFromToken(token);
+            return reportService.sendReportWithToken(reportService.mapToReport(reportDTO), request, itemID, userID);
+        }else{
+            return reportService.prepareSendReportWithoutToken(reportService.mapToReport(reportDTO), request, itemID);
+        }
+        
+    }
+
+    @PostMapping("/{itemID}/verify-and-send")
+    public ResponseEntity<Object> verifyAndSendReport(@Valid @RequestBody ValidateReportGuestDTO reportDTO,
+                                                      HttpServletRequest request, @PathVariable String itemID) {
+
+        return reportService.verifySendReportWithoutToken(reportDTO, request, itemID);
     }
 }
