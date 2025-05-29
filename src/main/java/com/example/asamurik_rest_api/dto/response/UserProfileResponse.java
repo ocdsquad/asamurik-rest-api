20ffb6ab39e3d056b853a588d2c210fc4f87d74e
ahmad alfarel <assaniry202@gmail.com>
2025-05-29 07:08:53 +0700

    feat: menambahkan DTO untuk response agar data penting tidak terleak

---
+++ src/main/java/com/example/asamurik_rest_api/dto/response/UserProfileResponse.java
@@ -0,0 +1,40 @@
+package com.example.asamurik_rest_api.dto.response;
+
+public class UserProfileResponse {
+    private String fullname;
+    private String email;
+    private String phoneNumber;
+    private String avatarUrl;
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+}
