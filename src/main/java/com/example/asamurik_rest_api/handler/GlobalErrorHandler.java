72eda920cabfcb58f6e6d0804dc1e3a2afca115f
ahmad alfarel <assaniry202@gmail.com>
2025-05-29 07:01:24 +0700

    refactor: merapihkan spasi

--- src/main/java/com/example/asamurik_rest_api/handler/GlobalErrorHandler.java
+++ src/main/java/com/example/asamurik_rest_api/handler/GlobalErrorHandler.java
@@ -7,26 +7,27 @@ import org.springframework.http.ResponseEntity;
 public class GlobalErrorHandler {
 
 
-    public static ResponseEntity<Object> dataGagalDisimpan(String errorCode , HttpServletRequest request){
-        return new ResponseHandler().handleResponse("FAILED TO SAVE !!", HttpStatus.INTERNAL_SERVER_ERROR,null,errorCode,request);
+    public static ResponseEntity<Object> dataGagalDisimpan(String errorCode, HttpServletRequest request) {
+        return new ResponseHandler().handleResponse("FAILED TO SAVE !!", HttpStatus.INTERNAL_SERVER_ERROR, null, errorCode, request);
     }
 
-    public static ResponseEntity<Object> dataGagalDiubah(String errorCode , HttpServletRequest request){
-        return new ResponseHandler().handleResponse("DATA GAGAL DIUBAH", HttpStatus.INTERNAL_SERVER_ERROR,null,errorCode,request);
+    public static ResponseEntity<Object> dataGagalDiubah(String errorCode, HttpServletRequest request) {
+        return new ResponseHandler().handleResponse("DATA GAGAL DIUBAH", HttpStatus.INTERNAL_SERVER_ERROR, null, errorCode, request);
     }
 
-    public static ResponseEntity<Object> dataGagalDihapus(String errorCode , HttpServletRequest request){
-        return new ResponseHandler().handleResponse("DATA GAGAL DIHAPUS", HttpStatus.INTERNAL_SERVER_ERROR,null,errorCode,request);
+    public static ResponseEntity<Object> dataGagalDihapus(String errorCode, HttpServletRequest request) {
+        return new ResponseHandler().handleResponse("DATA GAGAL DIHAPUS", HttpStatus.INTERNAL_SERVER_ERROR, null, errorCode, request);
     }
 
-    public static ResponseEntity<Object> terjadiKesalahan(String errorCode , HttpServletRequest request){
-        return new ResponseHandler().handleResponse("TERJADI KESALAHAN", HttpStatus.INTERNAL_SERVER_ERROR,null,errorCode,request);
+    public static ResponseEntity<Object> terjadiKesalahan(String errorCode, HttpServletRequest request) {
+        return new ResponseHandler().handleResponse("TERJADI KESALAHAN", HttpStatus.INTERNAL_SERVER_ERROR, null, errorCode, request);
     }
 
-    public static ResponseEntity<Object> dataTidakDitemukan(String errorCode , HttpServletRequest request){
-        return new ResponseHandler().handleResponse("DATA TIDAK DITEMUKAN", HttpStatus.BAD_REQUEST,null,errorCode,request);
+    public static ResponseEntity<Object> dataTidakDitemukan(String errorCode, HttpServletRequest request) {
+        return new ResponseHandler().handleResponse("DATA TIDAK DITEMUKAN", HttpStatus.BAD_REQUEST, null, errorCode, request);
     }
-    public static ResponseEntity<Object> objectIsNull(String errorCode , HttpServletRequest request){
-        return new ResponseHandler().handleResponse("OBJECT NULL !!", HttpStatus.BAD_REQUEST,null,errorCode,request);
+
+    public static ResponseEntity<Object> objectIsNull(String errorCode, HttpServletRequest request) {
+        return new ResponseHandler().handleResponse("OBJECT NULL !!", HttpStatus.BAD_REQUEST, null, errorCode, request);
     }
 }
