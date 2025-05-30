0d963349f4e6d0027155aae53bf55330ff58f326
ahmad alfarel <assaniry202@gmail.com>
2025-05-30 20:49:17 +0700

    refactor: merubah logic otp agar tidak mereturn 5 digit

--- src/main/java/com/asamurik_rest_api/utils/OtpGenerator.java
+++ src/main/java/com/asamurik_rest_api/utils/OtpGenerator.java
@@ -7,7 +7,9 @@ public class OtpGenerator {
     private static final Random random = new Random();
 
     public static String generateOtp() {
-        int otp = random.nextInt(999999);
-        return String.valueOf(otp);
+
+        int otp = random.nextInt(1_000_000);
+        return String.format("%06d", otp);
+
     }
 }
